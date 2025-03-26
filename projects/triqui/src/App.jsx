151b2c1cd981357a038664fc3328296170dc7287
daniel garcia <dany2017305@gmail.com>
2025-03-26 18:34:53 -0500

    Uso del local storage

--- projects/triqui/src/App.jsx
+++ projects/triqui/src/App.jsx
@@ -1,19 +1,27 @@
 import { useState } from 'react';
-import { Square } from './components/Square';
 import { WinnerModal } from './components/WinnerModal';
 import { ResetGame } from './components/ResetGame';
 import { Board } from './components/Board';
 import { TurnIndicator } from './components/TurnIndicator';
 
-import { TURNS } from './constants.jsx';
+import { TURNS } from './constants';
 import { checkWinnerFrom, checkEndGame } from './Logic/board';
+import { saveGameToStorage, resetGameFromStorage } from '../storage/index';
 
 import './index.css';
 import confetti from 'canvas-confetti';
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X); // Se usa directamente el elemento, sin paréntesis
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board');
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  });
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn');
+    return turnFromStorage ?? TURNS.X
+  });
+
   // null = no hay ganador, false = empate
   const [winner, setWinner] = useState(null);
 
@@ -30,6 +38,9 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
 
+    // Guarda la partida
+    saveGameToStorage({ board: newBoard, turn: newTurn });
+
     // Revisa si hay un ganador
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner) {
@@ -45,6 +56,9 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    resetGameFromStorage();
+
   };
 
   return (
